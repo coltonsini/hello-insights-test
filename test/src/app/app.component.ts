c8c74289eb3184bdd48d1843c6f8a978c9fe92a0
Juan José Ordoñez Villalobos <xxjuanjose15cxx@gmail.com>
2024-12-13 23:22:44 -0500

    Added the main fetch feature
    Added the fetch to the api to get the data into the template layout

--- test/src/app/app.component.ts
+++ test/src/app/app.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+
+
+export class AppComponent implements OnInit {
   title = 'Survey responses';
+  data: any;
+  groups: Group[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getData();
+  }
+
+  getData() {
+    this.http.get<ApiResponse>('https://hi-api-develop.helloinsight.org/tech-test-groups-list/')
+      .subscribe(response => {
+        this.data = response;
+        this.groups = response.data.groups;
+        // console.log(this.groups);
+      });
+  }
+}
+
+interface ApiResponse {
+  data: {
+    groups: Group[];
+  };
+}
+
+interface Group {
+  backup_surveys: string;
+  end_date: string;
+  id: number;
+  name: string;
+  participants: number;
+  response_target: number;
+  site: string;
+  start_date: string;
+  survey_admin_kit: string;
+  survey_date: string;
+  survey_link: string;
+  valid_responses: number;
 }
